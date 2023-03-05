137d1f89ec3ab7a93f703052f34c6b85a80fc1fe
vichheka <saovichhekakh@gmail.com>
2023-03-05 18:16:15 +0700

    version1

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+var items= [];
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get("/", function(req, res){
+
+    var today = new Date();
+    var options = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+    };
+    var day=today.toLocaleDateString("en-US", options);
+    
+    
+    res.render("list", {kindOfDay: day, newListItem: items});
+    
+});
+app.post("/", function(req, res){
+    var item = req.body.newItem;
+
+    items.push(item);
+
+    res.redirect("/");
+})
+
+app.listen(3000, function(){
+    console.log("Sever started on port 3000");
+});
